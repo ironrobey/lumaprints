2234c7ad17540494e2e79274c5f57421aadd17d5
Rob Go <mclordgt@gmail.com>
2020-09-10 20:07:15 +0800

    added sleep to cleanUptraits

--- app/Traits/CleanUpTraits.php
+++ app/Traits/CleanUpTraits.php
@@ -14,6 +14,7 @@ trait CleanUpTraits {
 	    $response = Http::get($url);
     	$output = json_decode($response);
     	$output = $output->output;
+    	sleep(60);
     	return HtmlDomParser::str_get_html($output->listingCards);
     }
 
